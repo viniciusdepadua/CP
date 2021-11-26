6f94d07f176dc66abdd519fe1976c99f1d4b441b
Vinicius <viniciusdepadua6@gmail.com>
2021-11-26 18:43:20 -0300

    6250

--- codeforces/CR757/1614A.cpp
+++ codeforces/CR757/1614A.cpp
@@ -2,3 +2,54 @@
 // Created by vinicius on 11/26/21.
 //
 
+
+#include<bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using db = long double;
+using pl = pair<ll,ll>;
+using pi = pair<int,int>;
+using cd = complex<db>;
+using vi = vector<int>;
+using vd = vector<db>;
+using vl = vector <ll>;
+
+int main() {
+    ios::sync_with_stdio(false);
+#ifndef NEAL_DEBUG
+    cin.tie(nullptr);
+#endif
+    int t;
+    cin >> t;
+    while(t--){
+        int n,l,r,k;
+        cin >> n >> l >> r >> k;
+        vl v(n);
+        for(int i = 0; i < n; i++){
+            int a;
+            cin >> a;
+            v[i] = a;
+
+        }
+        sort(v.begin(), v.end());
+        int total = 0;
+        int price = 0;
+        for(int i = 0; i < n; i++){
+            if(v[i] >= l && v[i] <= r){
+
+                price+=v[i];
+                if(price > k){
+                    break;
+                }
+                else{
+                    total++;
+                }
+            }
+
+        }
+        cout << total << endl;
+
+    }
+    return 0;
+}
--- codeforces/CR757/1614B.cpp
+++ codeforces/CR757/1614B.cpp
@@ -2,3 +2,59 @@
 // Created by vinicius on 11/26/21.
 //
 
+#include<bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using db = long double;
+using pl = pair<ll,ll>;
+using pi = pair<int,int>;
+using cd = complex<db>;
+using vi = vector<int>;
+using vd = vector<db>;
+using vl = vector <ll>;
+
+int main() {
+    ios::sync_with_stdio(false);
+#ifndef NEAL_DEBUG
+    cin.tie(nullptr);
+#endif
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        multimap<int,int> m;
+        vl ans(n);
+        for(int i = 0; i < n; i++){
+            ll a;
+            cin >> a;
+            m.insert(pair<ll,int>(-a,i));
+        }
+        ll positive = 1;
+        ll negative = -1;
+        int k = 0;
+        unsigned long long t = 0;
+        for(auto i = m.begin(); i!= m.end(); i++){
+            if(k%2 == 0){
+                ans[i->second] = positive;
+                t+=(unsigned long long) (abs(2 * positive * i->first));
+                positive++;
+            }
+            else{
+                ans[i->second] = negative;
+                t+=(unsigned long long) (abs(2 * negative * i->first));
+                negative--;
+            }
+            k++;
+        }
+        cout << t << endl;
+        cout << 0 << " ";
+        for(int i = 0; i < n; i++){
+            cout << ans[i] << " ";
+
+        }
+        cout << endl;
+    }
+    return 0;
+}
